694fd5a39967ae58841a6c08e3f328992b59984e
Glenardd <tolentinojanglenard5@gmail.com>
2024-11-04 18:58:00 +0800

    menu button size change

--- src/game/buttons/pauseButton/pauseBtn.jsx
+++ src/game/buttons/pauseButton/pauseBtn.jsx
@@ -7,11 +7,11 @@ export const pauseBtn = (scene, width, height) =>{
         scene.scene.launch("pauseMenu", {previousScene: scene.scene.key});
     };
 
-    const visibility = 0.1;
+    const visibility = 0;
 
     const addLine = new AddLine(scene, width, height);
-    const lineX = addLine.createVerticalLine(0.85, visibility).PosX; // Full visibility for vertical line
+    const lineX = addLine.createVerticalLine(0.88, visibility).PosX; // Full visibility for vertical line
     const lineY = addLine.createHorizontalLine(0.05, visibility).PosY; // Half visibility for horizontal line
 
-    scene.add.container(lineX, lineY).add(new ButtonCreate(scene,0,0, "Menu", 25, 50, 200, 0xe85f5f,0x914c4c, returnEvent, true));
+    scene.add.container(lineX, lineY).add(new ButtonCreate(scene,0,0, "Menu", 25, 50, 150, 0xe85f5f,0x914c4c, returnEvent, true));
 };
