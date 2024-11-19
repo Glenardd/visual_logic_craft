77b857e35dcab60b739e4dc36c7b88e96db124d6
Glenardd <tolentinojanglenard5@gmail.com>
2024-11-19 14:20:12 +0800

    added health bar class

---
+++ frontend/src/game/utils/healthBar.jsx
@@ -0,0 +1,23 @@
+class HealthBar extends Phaser.GameObjects.Container{
+    constructor(scene,x, y, value){
+        super(scene,x, y);
+
+        this.scene = scene;
+        this.healthValue = value;
+    };
+
+    //display the progress bar
+    displayHealthBar(){
+
+    };
+
+    //add health
+    Add(){
+
+    };
+
+    //subtract health
+    Subtract(){
+
+    };
+};
