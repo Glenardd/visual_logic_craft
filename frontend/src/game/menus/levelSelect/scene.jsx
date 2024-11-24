07609f0c5626c640f3f717bd5066ecaf0c8296e4
Glenardd <tolentinojanglenard5@gmail.com>
2024-11-25 03:53:28 +0800

    added bg images to level select

--- frontend/src/game/menus/levelSelect/scene.jsx
+++ frontend/src/game/menus/levelSelect/scene.jsx
@@ -12,10 +12,26 @@ import background_2 from "../../../assets/background_mission_two/Hills_Layer_01.
 import foreground_2 from "../../../assets/background_mission_two/Hills_Layer_02.png";
 import foreground_two_2 from "../../../assets/background_mission_two/Hills_Layer_03.png";
 
+//level select bg
+import forest_1 from "../../../assets/background_levelSelect/Layer_0011_0.png";
+import forest_2 from "../../../assets/background_levelSelect/Layer_0010_1.png";
+import forest_3 from "../../../assets/background_levelSelect/Layer_0009_2.png";
+import forest_4 from "../../../assets/background_levelSelect/Layer_0008_3.png";
+import forest_5 from "../../../assets/background_levelSelect/Layer_0007_Lights.png";
+import forest_6 from "../../../assets/background_levelSelect/Layer_0006_4.png";
+import forest_7 from "../../../assets/background_levelSelect/Layer_0005_5.png";
+import forest_8 from "../../../assets/background_levelSelect/Layer_0004_Lights.png";
+import forest_9 from "../../../assets/background_levelSelect/Layer_0003_6.png";
+import forest_10 from "../../../assets/background_levelSelect/Layer_0002_7.png";
+import forest_11 from "../../../assets/background_levelSelect/Layer_0001_8.png";
+import forest_12 from "../../../assets/background_levelSelect/Layer_0000_9.png";
+
 class LevelSelect extends Phaser.Scene{
     constructor(){
         super({key: "levelSelect"});
         this.changeScene = this.changeScene.bind(this);
+
+        this.assetLevelSelectKeys = [];
     };
 
     preload(){
@@ -26,6 +42,28 @@ class LevelSelect extends Phaser.Scene{
         this.load.image("background_2", background_2);
         this.load.image("foreground_2", foreground_2);
         this.load.image("foreground_two_2", foreground_two_2);
+
+        const assets = [
+            forest_1,
+            forest_2,
+            forest_3,
+            forest_4,
+            forest_5,
+            forest_6,
+            forest_7,
+            forest_8,
+            forest_9,
+            forest_10,
+            forest_11,
+            forest_12,
+        ];
+
+        assets.map((bg, i) =>{
+            this.load.image(`forest-${i}`, bg);
+
+            //push the level select keys
+            this.assetLevelSelectKeys.push(`forest-${i}`);
+        }); 
     };
 
     create(data){
@@ -67,6 +105,23 @@ class LevelSelect extends Phaser.Scene{
             foreground: "foreground_2",
             foreground_two: "foreground_two_2",
         };
+
+        //for level select bg
+        const bg = this.assetLevelSelectKeys;
+        const forest1 = this.add.image(0,0, bg[0]);
+        forest1.setScrollFactor(0);
+        forest1.setOrigin(0);
+        forest1.setDisplaySize(this.Width, this.Height);
+        forest1.setDepth(-1);
+        
+        bg.map((background, index) =>{
+            if(index !== 0){
+                const forest = this.add.tileSprite(0,-500, this.Width,this.Height,background);
+                forest.setOrigin(0);
+                forest.setScale(2.2);
+                forest.setDepth(-1);
+            };
+        });
 
         this.levelSelectbtn();
     };
