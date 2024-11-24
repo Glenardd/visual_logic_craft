50e28a6873b448e39c93f34ad8911afd104ed455
Glenardd <tolentinojanglenard5@gmail.com>
2024-11-24 11:42:50 +0800

    updated info about the getting the platform data

--- frontend/src/game/utils/addPlatforms.jsx
+++ frontend/src/game/utils/addPlatforms.jsx
@@ -12,7 +12,7 @@ class PlatformCreate {
     addPlatforms() {
         const width = this.scene.cameras.main.width;
         const height = this.scene.cameras.main.height;
-        
+
         const platformHeight = 700;
         const platformY = height * 1.2;
     
@@ -20,7 +20,7 @@ class PlatformCreate {
         const bridgeY1 = height * 0.8;
         const bridgeY2 = height * 0.9;
 
-        // Fetch platform data from the utility function
+        // Fetch from the platform data
         const platforms = this.missionMap(width, platformY, bridgeY1, bridgeY2, platformHeight);
 
         // Loop through the platform data and create platforms
