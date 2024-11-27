46b860012c9ade1cf35a5836364a45d843a0c66a
Glenardd <tolentinojanglenard5@gmail.com>
2024-11-28 02:08:43 +0800

    added a panel in card customization

--- frontend/src/game/scenes/menus/cardsCustomization/scene.jsx
+++ frontend/src/game/scenes/menus/cardsCustomization/scene.jsx
@@ -1,5 +1,6 @@
 import AddLine from "../../../utils/addLayoutGuide";
 import ReturnButton from "../../../utils/returnBtn";
+import cardData from "../../../objData/cardsData.json";
 
 class CardCustomization extends Phaser.Scene{
     constructor(){
@@ -14,6 +15,7 @@ class CardCustomization extends Phaser.Scene{
         this.visibility = 0;
         this.returnHome(width, height);
         this.title(width, height);
+        this.panel(width, height);
     };
 
     title(width, height){
@@ -34,6 +36,18 @@ class CardCustomization extends Phaser.Scene{
         const lineY = line.createHorizontalLine(0.03,this.visibility).PosY;
 
         new ReturnButton(this, lineX, lineY, "titleScreen");
+    };
+
+    panel(width, height){
+        const line = new AddLine(this, width, height);
+        const lineX = line.createVerticalLine(0.5,this.visibility).PosX;
+        const lineY = line.createHorizontalLine(0.5,this.visibility).PosY;
+        
+        const layout = this.add.container(lineX, lineY);
+
+        const panel = this.add.rectangle(0,0, width/2 + 600, height/2 + 300, 0x698a84);
+        panel.setStrokeStyle(4, 0x0000);
+        layout.add(panel);
     };
 
 };
