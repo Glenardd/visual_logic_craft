e439975fd9448889547a919eec1c4417d7af1add
Glenardd <tolentinojanglenard5@gmail.com>
2024-12-09 05:05:36 +0800

    added loding screen if game starts

--- frontend/src/game/scenes/menus/homeMenu/forestBg/scene.jsx
+++ frontend/src/game/scenes/menus/homeMenu/forestBg/scene.jsx
@@ -2,61 +2,89 @@
 import forestJson from "../../../../../assets/background_levelSelect/pixel_art_forest.json";
 import forestBg from "../../../../../assets/background_levelSelect/pixel_art_forest.png"
 
-class ForestBackGround extends Phaser.Scene{
-    constructor(){
-        super({key: "forestBackground"});
+class ForestBackGround extends Phaser.Scene {
+    constructor() {
+        super({ key: "forestBackground" });
         this.bg = [];
     };
 
-    preload(){
+    preload() {
+
+        this.cameras.main.setBackgroundColor('#1d1d1d');
+
+        const loadingText = this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2, 'LOADING...', {
+            fontSize: "50px",
+            align: 'center',
+        }).setOrigin(0.5);
+
+        this.rexUI.add.sizer({
+            x: this.cameras.main.width/2,
+            y: this.cameras.main.height/2,
+        }).add(
+            this.rexUI.add.label({
+                text: loadingText,
+            }),
+            "0",
+            "center"
+        ).layout();
+
+        this.load.on("progress", () => {
+            this.scene.stop("titleScreen"); // Stop the title screen once loading starts
+        });
+      
+        // Once loading is complete, launch the title screen (or the next scene)
+        this.load.on("complete", () => {
+            loadingText.destroy()
+            this.scene.launch("titleScreen");
+        });
+
         this.load.atlas("forest_bg", forestBg, forestJson);
     };
 
-    create(){
-
+    create() {
         const width = this.cameras.main.width;
         const height = this.cameras.main.height;
 
         //for title screen and level select bg
         this.forest_bg_frames = this.textures.get('forest_bg').getFrameNames().sort().reverse();
-        this.forest_bg_frames.map((forest,i) =>{
-            const x = this.textures.get('forest_bg').get(forest).x + width/2;
-            const y = this.textures.get('forest_bg').get(forest).y + height/2 - 450;
+        this.forest_bg_frames.map((forest, i) => {
+            const x = this.textures.get('forest_bg').get(forest).x + width / 2;
+            const y = this.textures.get('forest_bg').get(forest).y + height / 2 - 450;
             const textureWidth = this.textures.get('forest_bg').get(forest).width + width;
             const textureHeight = this.textures.get('forest_bg').get(forest).height;
 
-            const forest_bg = this.add.tileSprite(x,y,textureWidth ,textureHeight, "forest_bg", forest);
+            const forest_bg = this.add.tileSprite(x, y, textureWidth, textureHeight, "forest_bg", forest);
 
-            if(forest ===this.forest_bg_frames[2]){
+            if (forest === this.forest_bg_frames[2]) {
                 forest_bg.setScale(3.7);
             };
-            if(forest ===this.forest_bg_frames[3]){
+            if (forest === this.forest_bg_frames[3]) {
                 forest_bg.setScale(3.7);
             };
-            if(forest ===this.forest_bg_frames[4]){
+            if (forest === this.forest_bg_frames[4]) {
                 forest_bg.setScale(3.7);
             };
-            if(forest ===this.forest_bg_frames[5]){
+            if (forest === this.forest_bg_frames[5]) {
                 forest_bg.setScale(3.7);
             };
-            if(forest ===this.forest_bg_frames[6]){
+            if (forest === this.forest_bg_frames[6]) {
                 forest_bg.setScale(3.7);
             };
-            if(forest ===this.forest_bg_frames[7]){
+            if (forest === this.forest_bg_frames[7]) {
                 forest_bg.setScale(3.7);
             };
-            if(forest ===this.forest_bg_frames[8]){
+            if (forest === this.forest_bg_frames[8]) {
                 forest_bg.setScale(3.7);
             };
-            if(forest ===this.forest_bg_frames[9]){
+            if (forest === this.forest_bg_frames[9]) {
                 forest_bg.setScale(3.7);
                 forest_bg.y = -200;
             };
-            if(forest ===this.forest_bg_frames[10]){
+            if (forest === this.forest_bg_frames[10]) {
                 forest_bg.setScale(3.7);
                 forest_bg.y = y / textureHeight + height;
             };
-            if(forest ===this.forest_bg_frames[11]){
+            if (forest === this.forest_bg_frames[11]) {
                 forest_bg.setScale(3.7);
                 forest_bg.y = y / textureHeight + height;
             };
@@ -66,7 +94,7 @@ class ForestBackGround extends Phaser.Scene{
         });
     };
 
-    update(){
+    update() {
         this.bg.forEach(bg => { bg.sprite.tilePositionX += bg.speed; });
     };
 };
